bd258ddf8064ddee8c3b7e81598c37ad1fafa565
saiprasannasastry <saiprasannasastry@gmail.com>
2021-02-25 08:21:31 +0000

    pset1

---
+++ pset1/left_pyramid.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <cs50.h>
+
+
+void repeat(string s, int i);
+int main(void){
+
+    int s = 0;
+    while (true){
+    s = get_int("Height: ");
+    if (s >=1 && s<=23){
+        break;
+        }
+    }
+
+    for(int i = 1;i<=s;i++)
+    {
+
+       repeat("#",i);
+       printf("\n");
+    }
+}
+
+void repeat(string s, int i)
+{
+    for (int j=0;j<i;j++ ){
+        printf("%s",s);
+    }
+}
